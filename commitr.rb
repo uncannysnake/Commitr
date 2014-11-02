590f295fa3dd837e8fa8891d7417a56fe16192fe
nilsding <nilsding@nilsding.org>
2014-11-02 19:23:56 +0100

    kek

--- commitr.rb
+++ commitr.rb
@@ -1,13 +1,26 @@
 #!/usr/bin/env ruby
 
+mode = :date # can be :date or :count
 file_name="IE.txt"
 
-32768.times do |i|
-  File.open file_name, 'a' do |f|
-    f.puts i
+case mode
+when :date
+  start_date = Time.now
+  366.times do |i|
+    File.open file_name, 'a' do |f|
+      f.puts i
+    end
+    `git add #{file_name}`
+    `git commit -m "commit ##{i}" --date "#{(start_date - i*24*3600).to_s}"`
   end
-  `git add #{file_name}`
-  `git commit -m "commit ##{i}"`
+when :count
+  32768.times do |i|
+    File.open file_name, 'a' do |f|
+      f.puts i
+    end
+    `git add #{file_name}`
+    `git commit -m "commit ##{i}"`
+  end
 end
 
-`git push`
+# `git push`
